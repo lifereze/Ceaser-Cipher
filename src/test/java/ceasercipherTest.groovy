4a91df2630ec549ec238b8d0a032163e514fb838
lifereze <liferezearnold@gmail.com>
2020-06-30 10:11:11 +0300

    add the imports and the first test

--- src/test/java/ceasercipherTest.groovy
+++ src/test/java/ceasercipherTest.groovy
@@ -1,2 +1,25 @@
-class ceasercipherTest extends groovy.util.GroovyTestCase {
+package CeaserCipher;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class CeaserCipherTest{
+
+    @Test
+    public void rotateCharacter_ShiftsCharacterCorrectNumberofTimesAtEndOfAlphabet_char(){
+        char input='x';
+        char expectedOutput='h';
+        assertEquals(expectedOutput, CeaserCipher.Cipher(input, 10));
+    }
+
+    @Test
+    public void rotateCharacter_ShiftsCharacterCorrectNumberofTimes_char(){
+        char input='b';
+        char expectedOutput='i';
+
+        assertEquals(expectedOutput, CeaserCipher.Cipher(input, 7));
+    }
+
+
+
+
 }
